dfb92d071ed10af79a6cbe7dd5d671ff3be8ee62
Harerimana Egide <egideharerimana085@gmail.com>
2023-03-16 16:43:54 +0200

    fix(style): configure/enable Eslint and README
    - Configure and enable Eslint
    - Add Eslint to git pre-commit and pre-push hooks with husky
    - Edit README.md
    
    [Fixes #1]

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,27 @@
-import express, { Request, Response } from "express";
-import swaggerUI from "swagger-ui-express";
-import swaggerJsDoc from "swagger-jsdoc";
-import swaggerConfig from "../swagger.json";
-import morgan from "morgan";
-import cors from "cors";
-import { connectDB, sequelize } from "./db/config";
+import express, { Request, Response } from "express"
+import swaggerUI from "swagger-ui-express"
+import swaggerJsDoc from "swagger-jsdoc"
+import swaggerConfig from "../swagger.json"
+import morgan from "morgan"
+import cors from "cors"
+import { connectDB, sequelize } from "./db/config"
+import { config } from "dotenv"
 
-const app = express();
-const PORT = process.env.PORT || 4000;
+const app = express()
+const PORT = process.env.PORT || 4000
 
-app.use(cors({ origin: "*" }));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(morgan("tiny"));
+config({})
+
+app.use(cors({ origin: "*" }))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(morgan("tiny"))
 
 app.use(
-  "/api-docs",
-  swaggerUI.serve,
-  swaggerUI.setup(swaggerJsDoc(swaggerConfig))
-);
+    "/api-docs",
+    swaggerUI.serve,
+    swaggerUI.setup(swaggerJsDoc(swaggerConfig))
+)
 
 /**
  * @openapi
@@ -30,16 +33,16 @@ app.use(
  *         description: Server status and welcome message
  */
 app.get("", (req: Request, res: Response) => {
-  return res.status(200).send({
-    message: "Welcome to Phantom server",
-    serverStatus: "RUNNING",
-  });
-});
+    return res.status(200).send({
+        message: "Welcome to Phantom server",
+        serverStatus: "RUNNING",
+    })
+})
 
 app.listen(PORT, async () => {
-  console.info(`Server started at: http://localhost:${PORT}`);
-  await connectDB();
-  sequelize.sync({ force: false }).then(() => {
-    console.log("Synced database successfully...");
-  });
-});
+    console.info(`Server started at: http://localhost:${PORT}`)
+    await connectDB()
+    sequelize.sync({ force: false }).then(() => {
+        console.log("Synced database successfully...")
+    })
+})
