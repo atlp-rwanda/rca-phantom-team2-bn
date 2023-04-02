181bc9209e98577acfa7fea2ea9d5f204e288609
kelliaUmuhire <50176390+kelliaUmuhire@users.noreply.github.com>
2023-04-03 01:06:32 +0200

    feat(routes): change db config

--- src/db/config.ts
+++ src/db/config.ts
@@ -1,26 +1,20 @@
 import { Sequelize, DataTypes } from "sequelize";
 import dotenv from "dotenv";
 
-// const sequelize = new Sequelize({
-//     dialect: "postgres",
-//     host: String(process.env.DB_HOST),
-//     port: Number(process.env.DB_PORT),
-//     database: String(process.env.DB_NAME),
-//     username: String(process.env.DB_USER),
-//     password: String(process.env.DB_PASSWORD),
-// })
+dotenv.config();
+
+const sequelize = new Sequelize({
+  dialect: "postgres",
+  host: String(process.env.DB_HOST),
+  port: Number(process.env.DB_PORT),
+  database: String(process.env.DB_NAME),
+  username: String(process.env.DB_USER),
+  password: String(process.env.DB_PASSWORD),
+});
 
 // const sequelize = new Sequelize(
 //   "postgresql://postgres:Q5pusAHnukzzbQvJLqxi@containers-us-west-194.railway.app:7407/railway"
 // );
-const sequelize = new Sequelize({
-  dialect: "postgres",
-  host: "localhost",
-  port: 5432,
-  database: "railway",
-  username: "postgres",
-  password: "qwerty12345*",
-});
 
 async function connectDB() {
   try {
