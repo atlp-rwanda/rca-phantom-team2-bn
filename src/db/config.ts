1e1f700f3b65be67c101d8300c167fcd391db30e
kelliaUmuhire <50176390+kelliaUmuhire@users.noreply.github.com>
2023-04-03 11:23:46 +0200

    fx(check): attempt 3

--- src/db/config.ts
+++ src/db/config.ts
@@ -1,27 +1,34 @@
-import { Sequelize, DataTypes } from "sequelize"
+import { Sequelize, DataTypes } from "sequelize";
+import dotenv from "dotenv";
 
-const sequelize = new Sequelize({
-    dialect: "postgres",
-    host: String(process.env.DB_HOST),
-    port: Number(process.env.DB_PORT),
-    database: String(process.env.DB_NAME),
-    username: String(process.env.DB_USER),
-    password: String(process.env.DB_PASSWORD),
-    sync: {
-        force: false,
-        alter: {drop: false}
-    }
-})
+dotenv.config();
+
+const sequelize =
+  process.env.NODE_ENV === "test"
+    ? new Sequelize(
+        "postgresql://postgres:reaYBl4M1hbiDdxjnTdr@containers-us-west-194.railway.app:7407/railway"
+      )
+    : new Sequelize({
+        dialect: "postgres",
+        host: String(process.env.DB_HOST),
+        port: Number(process.env.DB_PORT),
+        database: String(process.env.DB_NAME),
+        username: String(process.env.DB_USER),
+        password: String(process.env.DB_PASSWORD),
+        sync: {
+          force: false,
+          alter: { drop: false },
+        },
+      });
 
 async function connectDB() {
-    try {
-        await sequelize.authenticate()
-        await sequelize.sync()
-        console.log("Successfully connected and synced the database")
-    } catch (error) {
-        console.error("Database connection error:", error)
-    }
+  try {
+    await sequelize.authenticate();
+    await sequelize.sync();
+    console.log("Successfully connected and synced the database");
+  } catch (error) {
+    console.error("Database connection error:", error);
+  }
 }
 
-export { connectDB, sequelize, Sequelize, DataTypes }
-
+export { connectDB, sequelize, Sequelize, DataTypes };
--- test/user.spec.ts
+++ test/user.spec.ts
@@ -1,62 +1,63 @@
-import request from "supertest"
-import app from "../src/app"
-import chai from "chai"
-import chaiHttp from "chai-http"
-import { createUser } from "../src/controllers/userController"
-import { connectDB } from "../src/db/config"
-const register = createUser
+import request from "supertest";
+import app from "../src/app";
+import chai from "chai";
+import chaiHttp from "chai-http";
+import { createUser } from "../src/controllers/userController";
+import { connectDB } from "../src/db/config";
+const register = createUser;
 
-const agent = request.agent(app)
+const agent = request.agent(app);
+let should = chai.should();
 
-chai.use(chaiHttp)
+chai.use(chaiHttp);
 
 const tUser = {
-    email: "tester@phantom.com",
-    password: "w6vav7",
-}
+  email: "tester@phantom.com",
+  password: "x36w5n",
+};
 
 describe("Users tests", () => {
-    before(async (done)=> {
-        await connectDB()
-        done()
-    })
+  //   before(async (done) => {
+  //     await connectDB();
+  //     done();
+  //   });
 
-    it("It should create user", function () {
-        request(register).get("/api/users").expect(200)
-    })
+  it("It should create user", function () {
+    request(register).get("/api/users").expect(200);
+  });
 
-    it("should sign in user", (done) => {
-        agent
-            .post("/api/users/signin")
-            .send(tUser)
-            .end((err, res) => {
-                res.should.have.status(200)
-                res.body.should.be.a("object")
-                res.body.should.have.property("data")
-                res.body.should.have.property("message").eq("Sign in successful")
-                done()
-            })
-    })
+  it("should sign in user", (done) => {
+    agent
+      .post("/api/users/signin")
+      .send(tUser)
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.body.should.be.a("object");
+        res.body.should.have.property("data");
+        res.body.should.have.property("message").eq("Sign in successful");
+        done();
+      });
+  });
 
-    it("user not found", (done) => {
-        agent
-            .post("/api/users/signin")
-            .send({ email: "ghost@mail.com", password: "12345" })
-            .end((err, res) => {
-                res.should.have.status(404)
-                res.body.should.have.property("message").eq("User not found")
-                done()
-            })
-    })
-    
-    it("invalid password", (done) => {
-        agent
-            .post("/api/users/signin")
-            .send({ email: "tester@phantom.com", password: "1290" })
-            .end((err, res) => {
-                res.should.have.status(401)
-                res.body.should.have.property("message").eq("Invalid message")
-                done()
-            })
-    })
-})
+  it("user not found", (done) => {
+    agent
+      .post("/api/users/signin")
+      .send({ email: "ghost@mail.com", password: "12345" })
+      .end((err, res) => {
+        res.should.have.status(404);
+        res.body.should.have.property("message").eq("User not found");
+        done();
+      });
+  });
+
+  it("invalid password", (done) => {
+    agent
+      .post("/api/users/signin")
+      .send({ email: "tester@phantom.com", password: "1290" })
+      .end((err, res) => {
+        res.should.have.status(401);
+        res.body.should.have.property("message").eq("Invalid message");
+        done();
+      });
+  });
+});
