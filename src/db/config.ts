39b0e683b4da9153bd80efd304ddcc01ec79740c
Harerimana Egide <egideharerimana085@gmail.com>
2023-04-03 12:10:34 +0200

    fix(authentication and testing): added interface for Express Request

--- src/db/config.ts
+++ src/db/config.ts
@@ -1,34 +1,35 @@
-import { Sequelize, DataTypes } from "sequelize";
-import dotenv from "dotenv";
+import { Sequelize, DataTypes } from "sequelize"
+import dotenv from "dotenv"
 
-dotenv.config();
+dotenv.config()
 
 const sequelize =
   process.env.NODE_ENV === "test"
-    ? new Sequelize(
-        "postgresql://postgres:reaYBl4M1hbiDdxjnTdr@containers-us-west-194.railway.app:7407/railway"
+      ? new Sequelize(
+          "postgresql://postgres:reaYBl4M1hbiDdxjnTdr@containers-us-west-194.railway.app:7407/railway"
       )
-    : new Sequelize({
-        dialect: "postgres",
-        host: String(process.env.DB_HOST),
-        port: Number(process.env.DB_PORT),
-        database: String(process.env.DB_NAME),
-        username: String(process.env.DB_USER),
-        password: String(process.env.DB_PASSWORD),
-        sync: {
-          force: false,
-          alter: { drop: false },
-        },
-      });
+      : new Sequelize({
+          dialect: "postgres",
+          host: String(process.env.DB_HOST),
+          port: Number(process.env.DB_PORT),
+          database: String(process.env.DB_NAME),
+          username: String(process.env.DB_USER),
+          password: String(process.env.DB_PASSWORD),
+          logging: false,
+          sync: {
+              force: false,
+              alter: { drop: false },
+          },
+      })
 
 async function connectDB() {
-  try {
-    await sequelize.authenticate();
-    await sequelize.sync();
-    console.log("Successfully connected and synced the database");
-  } catch (error) {
-    console.error("Database connection error:", error);
-  }
+    try {
+        await sequelize.authenticate()
+        await sequelize.sync()
+        console.log("Successfully connected and synced the database")
+    } catch (error) {
+        console.error("Database connection error:", error)
+    }
 }
 
-export { connectDB, sequelize, Sequelize, DataTypes };
+export { connectDB, sequelize, Sequelize, DataTypes }
