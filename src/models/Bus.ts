a9418e17618ff4ac1c90361cedb693974a2c100a
Harerimana Egide <egideharerimana085@gmail.com>
2023-04-03 13:30:06 +0200

    chore(buses): added Bus model

--- src/models/Bus.ts
+++ src/models/Bus.ts
@@ -1,14 +1,41 @@
 import { DataTypes, Model } from "sequelize"
 import { sequelize } from "../db/config"
 
-class Bus extends Model {}
+class Bus extends Model {
+    id!: string
+    type!: string
+    plateNumber!: string
+    regNumber!: string
+    model!: string
+    manufacturer!: string
+}
 
 Bus.init({
     id: {
         type: DataTypes.UUID,
         defaultValue: DataTypes.UUIDV4,
         primaryKey: true
-    }
+    },
+    type: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    plateNumber: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    regNumber: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    model: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    manufacturer: {
+        type: DataTypes.STRING,
+        allowNull: false
+    } 
 }, {
     sequelize: sequelize,
     timestamps: true,
