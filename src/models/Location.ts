7e09ea3bb3df8c6f5f5fe0d43e6e8e6b0a3fb62f
Harerimana Egide <egideharerimana085@gmail.com>
2023-04-05 11:29:20 +0200

    fis(documentation): fixed the documentation for routes

--- src/models/Location.ts
+++ src/models/Location.ts
@@ -1,39 +1,39 @@
-import { Model, DataTypes } from "sequelize";
-import { sequelize } from "../db/config";
+import { Model, DataTypes } from "sequelize"
+import { sequelize } from "../db/config"
 
 class Location extends Model {
-  public id!: number;
-  public name!: string;
-  public latitude!: number;
-  public longitude!: number;
-  public readonly createdAt!: Date;
-  public readonly updatedAt!: Date;
+    public id!: number
+    public name!: string
+    public latitude!: number
+    public longitude!: number
+    public readonly createdAt!: Date
+    public readonly updatedAt!: Date
 }
 
 Location.init(
-  {
-    id: {
-      type: DataTypes.UUID,
-      defaultValue: DataTypes.UUIDV4,
-      primaryKey: true,
+    {
+        id: {
+            type: DataTypes.UUID,
+            defaultValue: DataTypes.UUIDV4,
+            primaryKey: true,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        latitude: {
+            type: DataTypes.DECIMAL,
+            allowNull: false,
+        },
+        longitude: {
+            type: DataTypes.DECIMAL,
+            allowNull: false,
+        },
     },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    latitude: {
-      type: DataTypes.DECIMAL,
-      allowNull: false,
-    },
-    longitude: {
-      type: DataTypes.DECIMAL,
-      allowNull: false,
-    },
-  },
-  {
-    tableName: "Location",
-    sequelize,
-  }
-);
+    {
+        modelName: "Location",
+        sequelize,
+    }
+)
 
-export default Location;
+export default Location
