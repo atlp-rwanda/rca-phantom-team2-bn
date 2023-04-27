f373d45e72275a980b39eb5b835cd33f3a0032e8
joycyrus <isouvede2@gmail.com>
2023-04-27 15:16:47 +0200

    ft(inqure-bus-info): impliment view bus stattus, bus type and number commuters

--- src/socket.ts
+++ src/socket.ts
@@ -9,18 +9,46 @@ const io = new Server(server, {
     cors: {origin: "*"},
     transports: ["websocket"]
 })
-
+let lastAvailableSeats:number
+let availbleSeats:number
 interface LocationChange {
     busId: string;
+    busType: string;
     latitude: number;
     longitude: number;
 }
-
+interface CommuterChange{
+    busId: string;
+    busType: string;
+    alighted: number;
+    aboarded: number;
+}
+interface StatusChange{
+    busId: string;
+    busType: string;
+    status: string;
+    voidseats: number;
+}
 io.on("connection", (socket)=> {
     console.log("NEW CONNECTION:", socket.id)
     
     socket.on("BUS_LOCATION_CHANGE", (data: LocationChange)=> {
         io.emit("BUS_LOCATION_CHANGE", data)
+    })
+
+    socket.on("BUS_STATUS_CHANGE",(data:StatusChange)=>{
+        if (data.status==="START") lastAvailableSeats = data.voidseats
+
+        io.emit("BUS_STATUS_CHANGE",data)
+    })
+
+    socket.on("COMMUTERS_CHANGE",(data:CommuterChange)=>{
+        const numOfSeats = 28//this number will be got from backend busmodel        
+        availbleSeats = lastAvailableSeats-data.aboarded+data.alighted
+
+        const numOfCommuters:number = numOfSeats-availbleSeats
+        io.emit("COMMUTERS_AND_AVAILABLE_SEATS",{busId:data.busId,availbleSeats:availbleSeats,numOfCommuters:numOfCommuters})
+        lastAvailableSeats = availbleSeats
     })
 
     socket.on("disconnect", (reason)=> {
