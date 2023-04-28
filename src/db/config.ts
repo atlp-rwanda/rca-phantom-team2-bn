d59a305fed0c9f9a3e39e3e70e110fe2a40a8ee2
kelliaUmuhire <kellumuhire@gmail.com>
2023-04-28 06:21:45 -0700

    fx(db): enable ssl

--- src/db/config.ts
+++ src/db/config.ts
@@ -3,42 +3,31 @@ import dotenv from "dotenv"
 
 dotenv.config()
 
-// const sequelize =
-//   process.env.NODE_ENV === "test"
-//       ? new Sequelize(
-//           "postgres://phantom:x7HCMMAY9JKsniBAALdPND1TqqiqJCAD@dpg-ch5o2pl269v5rfrv4h0g-a.oregon-postgres.render.com/phantom_n3ha",
-//           {
-//               logging: false,
-//               sync: {
-//                   force: false,
-//                   alter: { drop: false },
-//               },
-//           }
-//       )
-//       : new Sequelize({
-//           dialect: "postgres",
-//           host: String(process.env.DB_HOST),
-//           port: Number(process.env.DB_PORT),
-//           database: String(process.env.DB_NAME),
-//           username: String(process.env.DB_USER),
-//           password: String(process.env.DB_PASSWORD),
-//           logging: false,
-//           sync: {
-//               force: false,
-//               alter: { drop: false },
-//           },
-//       })
-
-const sequelize = new Sequelize(
-    "postgres://phantom:x7HCMMAY9JKsniBAALdPND1TqqiqJCAD@dpg-ch5o2pl269v5rfrv4h0g-a.oregon-postgres.render.com/phantom_n3ha",
-    {
-        logging: false,
-        sync: {
-            force: false,
-            alter: { drop: false },
-        },
-    }
-)
+const sequelize =
+  process.env.NODE_ENV === "test"
+      ? new Sequelize(
+          "postgres://phantom:x7HCMMAY9JKsniBAALdPND1TqqiqJCAD@dpg-ch5o2pl269v5rfrv4h0g-a.oregon-postgres.render.com/phantom_n3ha?ssl=true",
+          {
+              logging: false,
+              sync: {
+                  force: false,
+                  alter: { drop: false },
+              },
+          }
+      )
+      : new Sequelize({
+          dialect: "postgres",
+          host: String(process.env.DB_HOST),
+          port: Number(process.env.DB_PORT),
+          database: String(process.env.DB_NAME),
+          username: String(process.env.DB_USER),
+          password: String(process.env.DB_PASSWORD),
+          logging: false,
+          sync: {
+              force: false,
+              alter: { drop: false },
+          },
+      })
 
 async function connectDB() {
     try {
