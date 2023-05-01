d385972d6355e1c8c96a30eb0a32dc48bb3282b3
pauline12ish34 <paulineishimwe12@gmail.com>
2023-05-01 21:57:53 +0200

    updated db configs

--- src/db/config.ts
+++ src/db/config.ts
@@ -1,41 +1,106 @@
+
+
 import { Sequelize, DataTypes } from "sequelize"
+
 import dotenv from "dotenv"
+
+​
 
 dotenv.config()
 
+​
+
 const sequelize =
+
   process.env.NODE_ENV === "test"
+
       ? new Sequelize(
-          "postgresql://postgres:reaYBl4M1hbiDdxjnTdr@containers-us-west-194.railway.app:7407/railway", {
+
+          "postgres://phantom:x7HCMMAY9JKsniBAALdPND1TqqiqJCAD@dpg-ch5o2pl269v5rfrv4h0g-a.oregon-postgres.render.com/phantom_n3ha",
+
+          {
+
               logging: false,
+
               sync: {
+
                   force: false,
+
                   alter: { drop: false },
+
               },
+
+              ssl: false,
+
+              dialectOptions: {
+
+                  ssl: {require: false}
+
+              }
+
           }
+
       )
+
       : new Sequelize({
+
           dialect: "postgres",
+
           host: String(process.env.DB_HOST),
+
           port: Number(process.env.DB_PORT),
+
           database: String(process.env.DB_NAME),
+
           username: String(process.env.DB_USER),
+
           password: String(process.env.DB_PASSWORD),
+
           logging: false,
+
           sync: {
+
               force: false,
+
               alter: { drop: false },
+
           },
+
+          ssl: false,
+
+          ...(process.env.NODE_ENV === "production"? {
+
+              dialectOptions: {
+
+                  ssl: {require: false}
+
+              }
+
+          }:{})
+
       })
 
+​
+
 async function connectDB() {
+
     try {
+
         await sequelize.authenticate()
+
         await sequelize.sync()
+
         console.log("Successfully connected and synced the database")
+
     } catch (error) {
+
         console.error("Database connection error:", error)
+
     }
+
 }
 
+​
+
 export { connectDB, sequelize, Sequelize, DataTypes }
+
